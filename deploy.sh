2efe6535f6fcf657b89a971b344543324aee3180
Hong Phuc Nguyen <xnohat@gmail.com>
2019-07-27 19:46:40 +0700

    Fix bug: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "Enter your github username (not email):"
 read githubusername
-php artisan down
 git reset --hard #carefully this command will destroy all changes of tracked files on server side code
 git pull https://$githubusername@github.com/xnohat/threader.git master:master
-
